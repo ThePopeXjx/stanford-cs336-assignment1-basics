4eee6d9455b2cfe440804c614945ad5bc4643def
ThePopeXjx <3101687357@qq.com>
2026-03-25 04:04:21 +0000

    Finish Tokenizer class implementation.

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import resource
 import sys
 import time
+from typing import Iterable, Iterator
 
 import regex as re
 from tqdm import tqdm
@@ -238,6 +239,116 @@ def train_bpe_openwebtext(
     print(f"- merges path  : {stats['merges_path']}")
     print("=" * 72)
     return stats
+
+
+class Tokenizer:
+    def __init__(self, vocab: dict[int, bytes],
+                 merges: list[tuple[bytes, bytes]],
+                 special_tokens: list[str] | None = None):
+        self.vocab = vocab
+        self.reverse_vocab = { v: k for k, v in vocab.items() }
+        self.merges = { t: i for i, t in enumerate(merges) }
+        self.special_tokens = special_tokens
+        if special_tokens:
+            escaped = [ re.escape(t) for t in sorted(special_tokens, key=len, reverse=True) ]
+            self.special_pat = re.compile("(" + "|".join(escaped) + ")")
+
+    @classmethod
+    def from_files(cls, vocab_filepath: str,
+                   merges_filepath: str,
+                   special_tokens: list[str] | None) -> "Tokenizer":
+        byte_encoder = gpt2_bytes_to_unicode()
+        byte_decoder = { v: k for k, v in byte_encoder.items() }
+        def decode_str_to_bytes(s: str) -> bytes:
+            b = bytes()
+            for c in s:
+                b += byte_decoder[c]
+            return b
+
+        with open(vocab_filepath, "r", encoding="utf-8") as f:
+            serialized_vocab = json.load(f)
+        vocab = {
+            idx: decode_str_to_bytes(str_repr)
+            for idx, str_repr in serialized_vocab.items()
+        }
+
+        merges = list()
+        with open(merges_filepath, "r", encoding="utf-8") as f:
+            for line in f:
+                pairs = line.rstrip("\n").split()
+                assert len(pairs) == 2
+                merges.append(decode_str_to_bytes(pairs[0]),
+                              decode_str_to_bytes(pairs[1]))
+        
+        return cls(vocab, merges, special_tokens)
+    
+    def encode(self, text: str) -> list[int]:
+        token_ids = list()
+        pre_tokens: list[str] = list()
+        if self.special_tokens:
+            start = 0
+            for m in self.special_pat.finditer(text):
+                # First, process plain texts before special tokens
+                if m.start() > start:
+                    pre_tokens.extend(re.findall(PAT, text[start:m.start()]))
+                # Retain special tokens
+                pre_tokens.append(m.group(0))
+                start = m.end()
+            # Finally, add plain texts after all special tokens.
+            if start < len(text):
+                pre_tokens.extend(re.findall(PAT, text[start:]))
+        else:
+            pre_tokens = re.findall(PAT, text)
+        for pre_token in pre_tokens:
+            if self.special_tokens and pre_token in self.special_tokens:
+                token_ids.append(self.reverse_vocab[pre_token.encode("utf-8")])
+            else:
+                pre_bytes = pre_token.encode("utf-8")
+                pre_bytes = [bytes([b]) for b in pre_bytes]
+                old_bytes = pre_bytes
+                # In naive implementation, we should iterate merges from top (high priority) to bottom (low priority).
+                # But consider the time complexity, it will be len(merges) * len(text) approximately, that's quite slow.
+                # In this implementation, I pre-process the initial merges to convert it into a priority dict.
+                # So that we don't have to iterate on the merge level.
+                # Just find the pair that has the highest priority in current sequence.
+                # Caution, the priority comparison is very important.
+                # If we only look up the pair in merges, it will produce the wrong result.
+                while True:
+                    if len(old_bytes) == 1:
+                        break
+                    orders = [
+                        self.merges.get(p, len(self.merges)) for p in zip(old_bytes, old_bytes[1:])
+                    ]
+                    max_order = min(orders)
+                    if max_order < len(self.merges):
+                        match_ids = [i for i, v in enumerate(orders) if v == max_order]
+                        new_bytes = list()
+                        i = 0
+                        while i < len(old_bytes):
+                            if i in match_ids:
+                                new_bytes.append(old_bytes[i] + old_bytes[i + 1])
+                                i += 2
+                            else:
+                                new_bytes.append(old_bytes[i])
+                                i += 1
+                        old_bytes = new_bytes
+                    else:
+                        break
+                for b in old_bytes:
+                    token_ids.append(self.reverse_vocab[b])
+        return token_ids
+    
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        for text in iterable:
+            token_ids = self.encode(text)
+            for id in token_ids:
+                yield id
+    
+    def decode(self, ids: list[int]) -> str:
+        b = bytes()
+        for id in ids:
+            b += self.vocab[id]
+        return b.decode(encoding="utf-8", errors="replace")
 
 
 if __name__ == "__main__":
